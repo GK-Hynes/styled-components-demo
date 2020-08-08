0a639ccc6acbc98bb7e8244882930942dfee2575
Gerard Hynes <gkhynes@gmail.com>
2020-08-08 19:09:12 +0100

    Add notes on props

--- README.md
+++ README.md
@@ -5,3 +5,11 @@ Getting better at styled-components with a little practice.
 styled-components creates scoped, reusable React components using tagged template literals.
 
 It outputs the element you specify and generates a unique class to style it.
+
+Like with other React components, you can pass props to a styled-component. You do this using string interpolation and an arrow function:
+
+```js
+const Button = styled.button`
+  background: ${({ type }) => (type === `cancel` ? `red` : `blue`)};
+`;
+```
--- src/App.js
+++ src/App.js
@@ -6,11 +6,23 @@ const Heading = styled.h1`
   font-size: 2rem;
 `;
 
+const Button = styled.button`
+  color: #fff;
+  font-size: 1.5rem;
+  padding: 0.5rem 1rem;
+  margin: 0.5rem;
+  border-radius: 0.25rem;
+  border: none;
+  background: ${({ type }) => (type === `cancel` ? `red` : `blue`)};
+`;
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Heading>Woo, tagged template literals!</Heading>
+        <Button>Save</Button>
+        <Button type="cancel">Cancel</Button>
       </header>
     </div>
   );
